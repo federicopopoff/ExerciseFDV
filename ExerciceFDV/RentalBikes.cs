914a5e58ef8beb802b9b97998c9f7630e428d163
federicopopoff <popofffederico@gmail.com>
2017-03-21 11:03:07 -0400

    Bike Rental Classes and Tests

---
+++ ExerciceFDV/RentalBikes.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciceFDV
+{
+    public interface IRental
+    {
+        string Description { get; }
+        double UnitCost { get; set; }
+        int cantRent { get; set; }
+
+        double CalcCost();
+    }
+
+    public abstract class SingleRental : IRental
+    {
+        public abstract int cantRent { get; set; }
+        public abstract string Description { get; }
+        public abstract double UnitCost { get; set; }
+
+        public double CalcCost()
+        {
+            return (cantRent * UnitCost);
+        }
+    }
+    
+    public class RentalHour : SingleRental
+    { 
+        public override string Description
+        {
+            get
+            {
+                return "Por Hora";
+            }          
+        }
+
+        private double _unitCost;
+
+        public override double UnitCost
+        {
+            get
+            {
+                return 5.00;
+            }
+            set
+            {
+                this._unitCost = value;
+            }
+        }
+
+
+        private int _cantRent;
+        public override int cantRent
+        {
+            get
+            {
+                return _cantRent;
+            }
+            set
+            {
+                this._cantRent = value;
+            }
+        }
+
+    }
+
+    public class RentalDay : SingleRental
+    {
+        public override string Description
+        {
+            get
+            {
+                return "Por Dia";
+            }
+        }
+
+        private double _unitCost;
+
+        public override double UnitCost
+        {
+            get
+            {
+                return 20.00;
+            }
+            set
+            {
+                this._unitCost = value;
+            }
+        }
+
+
+        private int _cantRent;
+        public override int cantRent
+        {
+            get
+            {
+                return _cantRent;
+            }
+            set
+            {
+                this._cantRent = value;
+            }
+        }
+       
+    }
+
+    public class RentalWeek : SingleRental
+    {
+        public override string Description
+        {
+            get
+            {
+                return "Por Semana";
+            }
+        }
+
+        private double _unitCost;
+
+        public override double UnitCost
+        {
+            get
+            {
+                return 60.00;
+            }
+            set
+            {
+                this._unitCost = value;
+            }
+        }
+
+
+        private int _cantRent;
+        public override int cantRent
+        {
+            get
+            {
+                return _cantRent;
+            }
+            set
+            {
+                this._cantRent = value;
+            }
+        }
+
+    }
+
+    public class FamilyRental : IRental
+    {
+        public string Description
+        {
+            get
+            {
+                return "Promocion";
+            }
+        }
+
+        private int _cantRent;
+        public int cantRent
+        {
+            get
+            {
+                return _cantRent;
+            }
+            set
+            {
+                this._cantRent = value;
+            }
+        }
+
+        private double _unitCost;
+        public double UnitCost
+        {
+            get
+            {
+                return _unitCost;
+            }
+            set
+            {
+                this._unitCost = value;
+            }
+        }
+
+        private List<IRental> Rentals;
+
+        public FamilyRental(List<IRental> rentals)
+        {
+            Rentals = rentals;
+        }
+        
+        public double CalcCost()
+        {
+            if(Rentals.Count == 0)
+                throw new Exception("Without rents");
+
+            if (Rentals.Count >= 3 && Rentals.Count <= 5)
+            {
+                double SummatoryCost = 0.00;
+
+                foreach (var item in Rentals)
+                {
+                    SummatoryCost += item.CalcCost();
+                }
+
+                UnitCost = SummatoryCost;
+
+                return Discount(UnitCost);
+            }
+            else
+                throw new Exception("Without promocion");
+
+        }
+
+        private double Discount(double cost)
+        {
+            return cost - cost * 0.3;
+        }
+
+    }
+
+   
+
+
+
+
+
+}
---
+++ UnitTestProject1/TestBikesRental.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExerciceFDV;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestBikesRental
+    {
+
+        [TestMethod]
+        public void RentalHourTest()
+        {
+
+            RentalHour rh = new RentalHour();
+
+            //Rent for 5 hours
+            rh.cantRent = 5;
+
+            //The function calculation for the total of the cost
+            double totalCost = rh.CalcCost();
+
+
+            Assert.AreEqual(totalCost, 25.00);
+
+
+        }
+
+        [TestMethod]
+        public void RentalDayTest()
+        {
+
+            RentalDay rd = new RentalDay();
+
+            //Rent for 2 days
+            rd.cantRent = 2;
+
+            //The function calculation for the total of the cost
+            double totalCost = rd.CalcCost();
+
+
+            Assert.AreEqual(totalCost, 40.00);
+
+
+        }
+
+        [TestMethod]
+        public void RentalWeekTest()
+        {
+
+            RentalWeek rd = new RentalWeek();
+
+            //Rent for 14 weeks
+            rd.cantRent = 14;
+
+            //The function calculation for the total of the cost
+            double totalCost = rd.CalcCost();
+
+
+            Assert.AreEqual(totalCost, 840.00);
+
+
+        }
+
+        [TestMethod]
+        public void RentalFamilyTest()
+        {
+            //Creates the collection of rentals
+            List<IRental> listRentals = new List<IRental>();
+
+            listRentals.Add(new RentalHour() { cantRent = 9 });
+            listRentals.Add(new RentalDay() { cantRent = 3 });
+            listRentals.Add(new RentalWeek() { cantRent = 12 });
+            
+            FamilyRental rf = new FamilyRental(listRentals);
+
+            //The function calculation for the total of the cost
+            double totalCost = rf.CalcCost();
+            
+            Assert.AreEqual(totalCost, 577.5);
+
+
+        }
+
+        [TestMethod]
+        public void RentalFamilyTestFailNoPromotion()
+        {
+            //Creates the collection of rentals
+            List<IRental> listRentals = new List<IRental>();
+
+            listRentals.Add(new RentalHour() { cantRent = 9 });
+            listRentals.Add(new RentalDay() { cantRent = 3 });
+
+            FamilyRental rf = new FamilyRental(listRentals);
+
+            try
+            {
+                //The function calculation for the total of the cost
+                double totalCost = rf.CalcCost();
+                Assert.Fail("Without promocion");
+            }
+            catch (Exception ex)
+            {
+                // whatever logging code
+            }
+           
+
+
+        }
+
+        [TestMethod]
+        public void RentalFamilyTestFailNoRentals()
+        {
+            //Creates the collection of rentals
+            List<IRental> listRentals = new List<IRental>();
+
+            FamilyRental rf = new FamilyRental(listRentals);
+
+            try
+            {
+                //The function calculation for the total of the cost
+                double totalCost = rf.CalcCost();
+                Assert.Fail("Without rents");
+            }
+            catch (Exception ex)
+            {
+                // whatever logging code
+            }
+
+
+
+        }
+    }
+}
